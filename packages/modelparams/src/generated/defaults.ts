f851c2310f03dc205cca3942c3bd129b56afdd55
Guillaume Gay <guillaume.gay@protonmail.com>
2026-08-07 04:35:58 +0000

    feat: add mistral/mistral-code-latest

--- packages/modelparams/src/generated/defaults.ts
+++ packages/modelparams/src/generated/defaults.ts
@@ -799,6 +799,13 @@ export const DEFAULTS = {
     "response_format.type": "text",
     safe_prompt: false,
   },
+  "mistral/mistral-code-latest": {
+    top_p: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0,
+    "response_format.type": "text",
+    safe_prompt: false,
+  },
   "mistral/mistral-large-latest": {
     top_p: 1,
     presence_penalty: 0,
