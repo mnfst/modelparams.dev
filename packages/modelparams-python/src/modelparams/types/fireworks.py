c76ad6b51c3d00711c401f0e844d1d65de25f07e
Guillaume Gay <guillaume.gay@protonmail.com>
2026-08-18 20:32:18 +0200

    feat: add fireworks/qwen3p8-2p4t-a95b (#272)

--- packages/modelparams-python/src/modelparams/types/fireworks.py
+++ packages/modelparams-python/src/modelparams/types/fireworks.py
@@ -130,6 +130,21 @@ Muse_Glimmer_30bParams = TypedDict(
 )
 setattr(Muse_Glimmer_30bParams, "__pydantic_config__", _PARAMS_CONFIG)
 
+Qwen3p8_2p4t_A95bParams = TypedDict(
+    "Qwen3p8_2p4t_A95bParams",
+    {
+        "max_tokens": Annotated[int, Field(ge=1)],
+        "temperature": Annotated[float, Field(ge=0, le=2)],
+        "top_p": Annotated[float, Field(ge=0, le=1)],
+        "top_k": Annotated[int, Field(ge=1, le=100)],
+        "presence_penalty": Annotated[float, Field(ge=-2, le=2)],
+        "frequency_penalty": Annotated[float, Field(ge=-2, le=2)],
+        "response_format.type": Literal["text", "json_object"],
+    },
+    total=False,
+)
+setattr(Qwen3p8_2p4t_A95bParams, "__pydantic_config__", _PARAMS_CONFIG)
+
 __all__ = [
     "Deepseek_V4_Flash_0731Params",
     "Deepseek_V4_Pro_0813Params",
@@ -139,4 +154,5 @@ __all__ = [
     "Kimi_K3Params",
     "Minimax_M2p7Params",
     "Muse_Glimmer_30bParams",
+    "Qwen3p8_2p4t_A95bParams",
 ]
