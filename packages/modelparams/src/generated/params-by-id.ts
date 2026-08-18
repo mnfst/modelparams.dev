ee4005d42ebcb58961e1d2371523145e45155d16
Guillaume Gay <guillaume.gay@protonmail.com>
2026-08-18 17:21:46 +0200

    feat: add alibaba/glm-5.2 (#245)

--- packages/modelparams/src/generated/params-by-id.ts
+++ packages/modelparams/src/generated/params-by-id.ts
@@ -55,6 +55,14 @@ export type ParamsById = {
     "extra_body.enable_thinking": boolean;
     "extra_body.thinking_budget": number;
   };
+  "alibaba/glm-5.2": {
+    max_completion_tokens: number;
+    temperature: number;
+    top_p: number;
+    "extra_body.top_k": number;
+    "extra_body.enable_thinking": boolean;
+    "extra_body.thinking_budget": number;
+  };
   "alibaba/kimi-k2.7-code": {
     max_completion_tokens: number;
     temperature: number;
