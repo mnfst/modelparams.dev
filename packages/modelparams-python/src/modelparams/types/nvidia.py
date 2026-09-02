3c1f43cd94068f56758f2b2e80cc02361457a2a7
Guillaume Gay <guillaume.gay@protonmail.com>
2026-09-02 20:09:53 +0000

    feat: add nvidia/openai/gpt-oss-120b

--- packages/modelparams-python/src/modelparams/types/nvidia.py
+++ packages/modelparams-python/src/modelparams/types/nvidia.py
@@ -37,6 +37,21 @@ Gliner_PiiParams = TypedDict(
 )
 setattr(Gliner_PiiParams, "__pydantic_config__", _PARAMS_CONFIG)
 
+Gpt_Oss_120bParams = TypedDict(
+    "Gpt_Oss_120bParams",
+    {
+        "temperature": Annotated[float, Field(ge=0, le=1)],
+        "top_p": Annotated[float, Field(le=1)],
+        "max_tokens": Annotated[int, Field(ge=1, le=16384)],
+        "frequency_penalty": Annotated[float, Field(ge=-2, le=2)],
+        "presence_penalty": Annotated[float, Field(ge=-2, le=2)],
+        "seed": Annotated[int, Field(ge=0, le=18446744073709552000)],
+        "stop": str,
+    },
+    total=False,
+)
+setattr(Gpt_Oss_120bParams, "__pydantic_config__", _PARAMS_CONFIG)
+
 Llama_3_1_Nemoguard_8b_Topic_ControlParams = TypedDict(
     "Llama_3_1_Nemoguard_8b_Topic_ControlParams",
     {
@@ -242,6 +257,7 @@ setattr(Usdcode_Llama_3_1_70b_InstructParams, "__pydantic_config__", _PARAMS_CON
 __all__ = [
     "Deepseek_V4_Flash_0731Params",
     "Gliner_PiiParams",
+    "Gpt_Oss_120bParams",
     "Llama_3_1_Nemoguard_8b_Topic_ControlParams",
     "Llama_3_1_Nemotron_Nano_8b_V1Params",
     "Llama_3_1_Nemotron_Safety_Guard_8b_V3Params",
